9d9eb99923fbc62b88657f55a81af864d9eac1e9
36892883 <36892883@qq.com>
2020-03-24 21:17:43 +0800

    todo

---
+++ readme.txt
@@ -0,0 +1,4 @@
+# TODO
+- RBAC
+- validation
+- front end
--- src/main/java/com/github/spec/DomainField.java
+++ src/main/java/com/github/spec/DomainField.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DomainField {
     private String type;
     private String value;
-    private DomainFieldValidation validation;
+    private List<DomainFieldValidation> validations;
 }
